c163ea7de159af6cf00a7c57f30fe64bef1dcf7c
NowWeAre-Free <kontodarm@wp.pl>
2023-06-05 20:23:52 +0200

    deleted state with static data, add new array with objects

--- array-operations/src/App.js
+++ array-operations/src/App.js
@@ -1,6 +1,31 @@
 import React from "react";
 import "./App.css";
 
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: "Arek",
+    },
+    {
+      id: 2,
+      age: 49,
+      name: "Marta",
+    },
+    {
+      id: 3,
+      age: 19,
+      name: "Stasia",
+    },
+    {
+      id: 4,
+      age: 24,
+      name: "Karol",
+    },
+  ],
+};
+
 const Item = (props) => <li>{`owoc ${props.content}`}</li>;
 
 class ListItems extends React.Component {
@@ -8,11 +33,9 @@ class ListItems extends React.Component {
     items: ["jabłko", "śliwka", "gruszka"],
   };
   render() {
-    const Items = this.state.items.map((item) => (
-      <Item key={item} content={item} />
-    ));
+    const users = this.users.map((item) => <Item key={item} content={item} />);
 
     return <ul>{Items}</ul>;
   }
 }
-export default ListItems;
+export default <ListItems data={data} />;
