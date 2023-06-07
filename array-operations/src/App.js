c28f692f39a58c987b1f4078936051d30c3f469a
NowWeAre-Free <kontodarm@wp.pl>
2023-06-07 22:35:39 +0200

    modified Item component by adding some new p tags

--- array-operations/src/App.js
+++ array-operations/src/App.js
@@ -4,43 +4,44 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
+      sex: "male",
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
+      sex: "female",
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
+      sex: "female",
     },
     {
       id: 4,
       age: 24,
       name: "Karol",
-    }
-  ]
-}
+      sex: "male",
+    },
+  ],
+};
 
 const Item = ({ user }) => (
-  <div>
-    <h1>Użytkownik {user.name}</h1>
-    <h2>Ma {user.age}</h2>
+  <div className="userInfo">
+    <h1>{user.name}</h1>
+    <p>Informacje o użytkowniku</p>
+    <h2>Wiek użytkownika: {user.age}</h2>
+    <p>Płeć użytkownika: {user.sex}</p>
   </div>
-)
+);
 class ListItems extends React.Component {
-
   render() {
-    const users = this.props.data.users
-    const Items = users.map(user => <Item key={user.id} user={user} />)
+    const users = this.props.data.users;
+    const Items = users.map((user) => <Item key={user.id} user={user} />);
 
-    return (
-      <ul>
-        {Items}
-      </ul>
-    )
+    return <ul>{Items}</ul>;
   }
 }
 
-ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
