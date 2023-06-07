fc437f6866f33bd311f633ebf51069e5a109b9bd
NowWeAre-Free <kontodarm@wp.pl>
2023-06-07 22:15:21 +0200

    add sex to every person in list

--- array-operations/src/App.js
+++ array-operations/src/App.js
@@ -1,18 +1,46 @@
-import React from "react";
-import "./App.css";
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: "Arek",
+    },
+    {
+      id: 2,
+      age: 49,
+      name: "Marta",
+    },
+    {
+      id: 3,
+      age: 19,
+      name: "Stasia",
+    },
+    {
+      id: 4,
+      age: 24,
+      name: "Karol",
+    }
+  ]
+}
 
-const Item = (props) => <li>{`owoc ${props.content}`}</li>;
-
+const Item = ({ user }) => (
+  <div>
+    <h1>Użytkownik {user.name}</h1>
+    <h2>Ma {user.age}</h2>
+  </div>
+)
 class ListItems extends React.Component {
-  state = {
-    items: ["jabłko", "śliwka", "gruszka"],
-  };
-  render() {
-    const Items = this.state.items.map((item) => (
-      <Item key={item} content={item} />
-    ));
 
-    return <ul>{Items}</ul>;
+  render() {
+    const users = this.props.data.users
+    const Items = users.map(user => <Item key={user.id} user={user} />)
+
+    return (
+      <ul>
+        {Items}
+      </ul>
+    )
   }
 }
-export default ListItems;
+
+ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
